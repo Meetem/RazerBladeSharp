8cd1009316a320ed89d48aca3279d7ca6863c8dc
Alex <hawk0044@gmail.com>
2022-07-23 19:17:03 +0200

    Add overclock parsing

--- Simple/Program.cs
+++ Simple/Program.cs
@@ -9,11 +9,14 @@ namespace RazerBladeSharp
     {
         private const string keyBright = "keybright=";
         private const string keyColor = "keycolor=";
+        private const string cpuBoostKey = "cpuboost=";
+        private const string gpuBoostKey = "gpuboost=";
+
         private const double percentToByte = 255.0 / 100.0;
         public static float RetryInterval = 1f;
         private static Laptop _laptop;
         private static UsbDevice _usbDevice;
-            
+
         private static int ParsePowerMode(string str)
         {
             if (string.IsNullOrEmpty(str))
@@ -33,17 +36,17 @@ namespace RazerBladeSharp
 
                 return pm;
             }
-            
+
             return 0;
         }
 
         private static void QueryAndPrintDescription(bool printLaptop)
         {
             var desc = _laptop.GetDescription();
-            
-            if(printLaptop)
+
+            if (printLaptop)
                 Console.WriteLine($"Found laptop {desc.name}, fan = {desc.fan}, capabilities = {desc.capabilities}");
-            
+
             Console.WriteLine("Requesting status");
 
             //Simple query of current state, will be written in internal Laptop* object
@@ -66,7 +69,31 @@ namespace RazerBladeSharp
                 $"\n\tManualFanSpeed: {status.IsManualFanSpeed}" +
                 $"\n\tKeyboardBrightness: {status.keyboardInfo.brightness}");
         }
-        
+
+        private static bool ParseKey(string str, string prefix, out string value)
+        {
+            if (!str.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+            {
+                value = null;
+                return false;
+            }
+
+            value = str.Substring(prefix.Length);
+            return value.Length > 0;
+        }
+
+        private static bool ParseKeyInt(string str, string prefix, out int i)
+        {
+            i = 0;
+            if (!ParseKey(str, prefix, out var s))
+                return false;
+
+            if (!int.TryParse(s, out i))
+                return false;
+
+            return true;
+        }
+
         public static void Main(string[] args)
         {
             if (args.Length < 1)
@@ -74,7 +101,7 @@ namespace RazerBladeSharp
                 Console.WriteLine("Usage: Simple.exe FanSpeed [powerMode:balanced/custom] [FanSpeed 2]");
                 return;
             }
-            
+
             UsbInterface.Initialize();
             AppDomain.CurrentDomain.ProcessExit += OnExit;
 
@@ -84,7 +111,7 @@ namespace RazerBladeSharp
                 QueryAndPrintDescription(true);
                 return;
             }
-            
+
             var fanSpeed = FanSpeed.Parse(args[0]);
             int powerMode = ParsePowerMode(args.Length >= 2 ? args[1] : null);
             var fanSpeed2 = FanSpeed.Parse(args.Length >= 3 ? args[2] : null);
@@ -92,32 +119,40 @@ namespace RazerBladeSharp
             byte kbBrightness = 32;
             bool disableGetDescription = false;
             int[] cp = new int[3] { 30, 255, 30 };
-            
+            int cpuBoost = 0;
+            int gpuBoost = 0;
+
             for (int i = 0; i < args.Length; i++)
             {
                 var a = args[i]?.Trim();
-                if(string.IsNullOrEmpty(a))
+                if (string.IsNullOrEmpty(a))
                     continue;
 
                 if (a.Equals("disable_get_description"))
                     disableGetDescription = true;
 
-                if (a.StartsWith(keyBright))
+                if (ParseKeyInt(a, keyBright, out var kbdb))
                 {
-                    a = a.Substring(keyBright.Length);
-                    if (int.TryParse(a, out var kbdb))
-                    {
-                        kbBrightness = (byte)Math.Max(Math.Min((int)(kbdb * percentToByte), 255), 0);
-                        Console.WriteLine($"Parsed keyboard brightness {kbBrightness / percentToByte:0.00}%");
-                    }
-
+                    kbBrightness = (byte)Math.Max(Math.Min((int)(kbdb * percentToByte), 255), 0);
+                    Console.WriteLine($"Parsed keyboard brightness {kbBrightness / percentToByte:0.00}%");
                     continue;
                 }
 
-                if (a.StartsWith(keyColor))
+                if (ParseKeyInt(a, cpuBoostKey, out var cb))
                 {
-                    a = a.Substring(keyColor.Length);
-                    var colors = a.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                    cpuBoost = cb;
+                    continue;
+                }
+                
+                if (ParseKeyInt(a, gpuBoostKey, out var gb))
+                {
+                    gpuBoost = gb;
+                    continue;
+                }
+
+                if (ParseKey(a, keyColor, out var st))
+                {
+                    var colors = st.Split(",", StringSplitOptions.RemoveEmptyEntries);
                     if (colors.Length != 3)
                     {
                         Console.WriteLine("Key colors invalid, example is 30,255,30");
@@ -130,16 +165,20 @@ namespace RazerBladeSharp
                         if (int.TryParse(colors[j], out cp[j]))
                             cp[j] = Math.Clamp(cp[j], 0, 255);
                     }
-                    
+
                     Console.WriteLine($"Parsed key color rgb({cp[0]}, {cp[1]}, {cp[2]})");
+                    continue;
                 }
             }
-            
+
             Console.WriteLine($"Parsed: Speed: {fanSpeed}" +
                               $", PowerMode: {powerMode}" +
                               $", Second Fan Speed {fanSpeed2}" +
                               $", KeyBrightness: {kbBrightness / percentToByte:0.00}%" +
-                              $", Key Colors: ({cp[0]}, {cp[1]}, {cp[2]})");
+                              $", Key Colors: ({cp[0]}, {cp[1]}, {cp[2]})" + 
+                              $", CpuBoost {cpuBoost}" + 
+                              $", GpuBoost {gpuBoost}" 
+                              );
 
             DiscoverLaptop();
 
@@ -150,17 +189,19 @@ namespace RazerBladeSharp
             {
                 QueryAndPrintDescription(true);
             }
-            
+
             /*
              * This effectively setting 5000RPM manually,
              * AND selecting Gaming power mode (0 - Balanced, 1 - Gaming, 2 - Creator (only supported on some laptops))
              */
             _laptop.SetFanSpeed(fanSpeed.GetSpeed(), 0, 1, fanSpeed.clamp);
-            
+
             if (fanSpeed2.hasValue)
                 _laptop.SetFanSpeed(fanSpeed2.GetSpeed(), 0, 2, fanSpeed2.clamp);
-            
+
             _laptop.SetPowerMode((byte)powerMode, fanSpeed.GetSpeed() == 0);
+            _laptop.SetBoost(BladeBoostId.Cpu, (byte)cpuBoost, 4);
+            _laptop.SetBoost(BladeBoostId.Gpu, (byte)gpuBoost, 4);
             _laptop.SetBrightness(kbBrightness);
             
             for (int i = 0; i < 6; i++)
@@ -168,20 +209,20 @@ namespace RazerBladeSharp
                 var row = new KeyboardRow((byte)i);
                 for (int j = 0; j < 15; j++)
                     row.keys[j] = new Rgb24((byte)cp[0], (byte)cp[1], (byte)cp[2]);
-                
+
                 _laptop.SendKeyboardRow(row);
             }
 
             //Apply sent colors
             _laptop.ApplyChroma();
-            
+
             if (!disableGetDescription)
             {
                 Thread.Sleep(1000);
                 Console.WriteLine("Status after set:");
                 QueryAndPrintDescription(false);
             }
-            
+
             Console.WriteLine("Set. Exiting in 5 seconds");
             Thread.Sleep(5000);
         }
